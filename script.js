88331b7a178ef63cbd7942e0b355fa359808ec99
Max00T <trousselmaxime@gmail.com>
2024-11-28 17:21:55 +0100

    modif html pour ajout audio et fusion style css

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // ajout des deux autres équipement
+
+// son sur équipement
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Sélection image 
+    const equipementImage = document.getElementById('equipementImage');
+    // Sélection l'audio
+    const SoundCo2 = document.getElementById('SoundCo2');
+  
+    // Ajout clic sur l'image
+    equipementImage.addEventListener('click', () => {
+      // play lorsque l'image est cliquée
+      SoundCo2.play();
+    });
+  });
+  
