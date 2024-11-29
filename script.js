f17726a2659ce413e4455fa6d43dc900ed6fc1df
Alex Taranne <alextaranne@gmail.com>
2024-11-29 09:54:54 +0100

    Modification du header

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// ajout des deux autres équipement
+// // ajout des deux autres équipement
+// const h2 = document.querySelector("h2")
+// // Ajout de la class numberClick a span
+// const span = document.querySelector("span");
+// span.classList.add("numberClick");
+// // const numberClick = span.classList.add("numberClick");
+// h2.appendChild(numberClick);
+const hamster = document.getElementsByClassName(".hamster")
+const score = 0;
+const countClick = 1;
+
+const addScore = function(){
+    if (score >= 0) {
+        score = score + countClick;
+        numberClick.innerHTML = score;
+    }
+
+}
+hamster.add
+
