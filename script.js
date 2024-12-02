4bc2ef481a81f582590b5d7ea4f1ce0f6074fa05
Ochoooooo <greenfieldrp7@gmail.com>
2024-12-02 14:27:53 +0100

    update fils

--- script.js
+++ script.js
@@ -66,6 +66,28 @@ function createEquipement(equipements) {
 
 createEquipement(equipementsSpatials);
 
+const bar = new ProgressBar.Circle("#progress-bar", {
+  color: "#4caf50",
+  strokeWidth: 10,
+  trailColor: "#",
+  trailWidth: 5,
+  duration: 1400,
+  easing: "easeInOut",
+  from: { color: "#FEB310", width: 3 },
+  to: { color: "#f44336", width: 3 },
+  step: function (state, circle) {
+    circle.path.setAttribute("stroke", state.color);
+    circle.path.setAttribute("stroke-width", state.width);
+    circle.path.setAttribute("stroke-linecap", "round");
+  },
+});
+
+function updateProgress(progress) {
+  bar.set(progress / 100);
+}
+
+updateProgress(50);
+
 // ajout des deux autres équipement
 
 // son sur équipement
